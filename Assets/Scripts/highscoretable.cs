cfe4a8bb4c5b2e7eb537cc37ce2f420cd2ca8fb7
Allan Cruvinel <allan.cruvinel@gmail.com>
2020-03-21 00:00:17 -0300

    terminad estrutura inicial de Scores

--- Assets/Scripts/highscoretable.cs
+++ Assets/Scripts/highscoretable.cs
@@ -9,20 +9,55 @@ public class highscoretable : MonoBehaviour
 
     private Transform entryContainer;
     private Transform entryTemplate;
+    float templateHeight = 65f;
+    private List<HighScoreEntry> highscoreEntrylist;
+    private List<Transform> highScoreEntryTranformList;
     private void Start() {
         entryContainer = transform.Find("highscoreEntryContainer");
         entryTemplate = entryContainer.Find("highscoreEntryTemplate");
         Debug.Log("entrou no Awake");
-        entryTemplate.gameObject.SetActive(false);
-        float templateHeight = 65f;
+        entryTemplate.gameObject.SetActive(false);  
 
-        for(int i =0; i<10;i++){
-            Transform entryTransform = Instantiate(entryTemplate,entryContainer);
+        highscoreEntrylist = new List<HighScoreEntry>(){
+            new HighScoreEntry{score = 51245},
+            new HighScoreEntry{score = 4854},
+            new HighScoreEntry{score = 12456},
+            new HighScoreEntry{score = 89654},
+            new HighScoreEntry{score = 12478},
+            new HighScoreEntry{score = 65821},
+            new HighScoreEntry{score = 77412},
+            new HighScoreEntry{score = 98654}
+        };
+        highscoreEntrylist.Add(new HighScoreEntry(10000));
+        highscoreEntrylist.Add(new HighScoreEntry(99999));
+        highscoreEntrylist.Add(new HighScoreEntry(99999));
+
+        for(int i =0;i<highscoreEntrylist.Count;i++){
+            for(int j = i + 1; j<highscoreEntrylist.Count;j++){
+                if(highscoreEntrylist[j].score>highscoreEntrylist[i].score){
+                    HighScoreEntry aux = highscoreEntrylist[i];
+                    highscoreEntrylist[i] = highscoreEntrylist[j];
+                    highscoreEntrylist[j]= aux;
+                }
+            }
+        }
+
+        highScoreEntryTranformList = new List<Transform>();
+        foreach(HighScoreEntry highScoreEntry in highscoreEntrylist){
+            CreateHighCoreEntryTransform(highScoreEntry,entryContainer,highScoreEntryTranformList);
+        }
+
+
+    }
+
+    private void CreateHighCoreEntryTransform(HighScoreEntry highScoreEntry, Transform container, List<Transform> transformlist){
+            float templateHeight = 65f;
+            Transform entryTransform = Instantiate(entryTemplate,container);
             RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
-            entryRectTransform.anchoredPosition = new Vector2(0,-templateHeight*i);
-            entryTemplate.gameObject.SetActive(true);
+            entryRectTransform.anchoredPosition = new Vector2(0,-templateHeight*transformlist.Count);
+            entryTransform.gameObject.SetActive(true);
 
-            int rank = i+1;
+            int rank = transformlist.Count+1;
             string rankString;
             switch(rank){
                 default: rankString = rank + "TH";break;
@@ -31,10 +66,23 @@ public class highscoretable : MonoBehaviour
                 case 3: rankString = "3RD";break;
             }
 
-            entryTransform.Find("postext").GetComponent<Text>().text = "";
+            entryTransform.Find("postext").GetComponent<Text>().text = rankString;
 
+            int Score = highScoreEntry.score;
+
+            entryTransform.Find("scoretext").GetComponent<Text>().text = Score.ToString();
+
+            transformlist.Add(entryTransform);
+    }
+
+    private class HighScoreEntry{
+        public int score;
+
+        public HighScoreEntry(){
             
         }
-        
+        public HighScoreEntry(int Score){
+            this.score = Score;
+        }
     }
 }
